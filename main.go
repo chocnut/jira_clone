d49d4c9ff4fcaa428bea41a44379a5b082c37d27
Peter Indiola <pindiola@gmail.com>
2020-05-21 21:18:49 +0800

    Added port handling

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/chocnut/jira_clone/controllers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Fatal("$PORT must be set")
+	}
+
 	handler := http.NewServeMux()
 
 	handler.HandleFunc("/ping", controllers.Ping)
-	err := http.ListenAndServe(":8080", handler)
+	err := http.ListenAndServe(port, handler)
 
 	if err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
