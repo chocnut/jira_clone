15cae36a72145183c280770af0f36642d6ac8203
Peter Indiola <pindiola@gmail.com>
2020-05-29 17:38:56 +0800

    using sqldb

--- api/controllers/projects_controller.go
+++ api/controllers/projects_controller.go
@@ -1,19 +1,42 @@
 package controllers
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
-	"time"
 
 	"github.com/chocnut/jira_clone/api/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func GetAllProjects(w http.ResponseWriter, r *http.Request) {
-	var Projects = []models.Project{
-		models.Project{ID: 1, Name: "Sample Project 1", CreatedAt: time.Now()},
-		models.Project{ID: 2, Name: "Sample Project 2", CreatedAt: time.Now()},
+
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/jira_clone?parseTime=true")
+	if err != nil {
+		panic(err.Error())
 	}
+
+	rows, err := db.Query("SELECT id, name, description, created_at, updated_at FROM projects")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var projects []models.Project
+
+	for rows.Next() {
+		var project models.Project
+
+		if err := rows.Scan(&project.ID, &project.Name, &project.Description, &project.CreatedAt, &project.UpdatedAt); err != nil {
+			log.Fatal(err)
+		}
+
+		projects = append(projects, project)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(Projects)
+	json.NewEncoder(w).Encode(projects)
 }
